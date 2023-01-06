d8dd49cd4adb22313cc3d150e3efa148605e5754
Kala Foods <guhan.delta@gmail.com>
2023-01-06 15:53:53 -0500

    Added Redux Persist to persist the state of the app, even after the page/session refresh

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App';
 import store from './store/store'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
+const persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
 import CartReducer from "./slices/CartSlice";
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage
+}
+
+const reducer = combineReducers({ 
+    cart: CartReducer
+});
+
+const persistedReducer = persistReducer( persistConfig, reducer );
+
 const store = configureStore({
-    reducer:{
-        cart: CartReducer
-    },
+    reducer: persistedReducer
 });
 
 export default store;
